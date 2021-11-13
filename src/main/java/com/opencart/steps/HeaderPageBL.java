dfde2912a8ca59944134abb2407925078770462d
Sviatoslav507 <92598985+Sviatoslav507@users.noreply.github.com>
2021-11-13 14:24:54 +0200

    New business logic update

--- src/main/java/com/opencart/steps/HeaderPageBL.java
+++ src/main/java/com/opencart/steps/HeaderPageBL.java
@@ -1,7 +1,12 @@
 package com.opencart.steps;
 
+import com.opencart.driver.DriverRepository;
+import com.opencart.enums.Currencies;
+import com.opencart.pages.CartPage;
 import com.opencart.pages.HeaderPage;
 import com.opencart.pages.SearchPage;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HeaderPageBL {
 
@@ -17,15 +22,46 @@ public class HeaderPageBL {
     }
 
     public RegisterPageBL clickOnRegisterButton() {
-        headerPage.getRegisterButton().click();
+        headerPage.getHeaderRegisterButton().click();
         return new RegisterPageBL();
     }
 
-    public SearchPage useSearch(String product){
+    public LoginPageBL clickOnLoginButton(){
+        headerPage.getHeaderLoginButton().click();
+        return new LoginPageBL();
+    }
+
+    public CartPageBL clickOnHeaderUpperCartButton(){
+        headerPage.getHeaderUpperCart().click();
+        return new CartPageBL();
+    }
+
+    /*public SharePageBL clickOnHeaderShareButton(){
+        headerPage.getHeaderShareButton().click();
+        return new SharePageBL();
+    }*/
+
+    public HeaderPageBL headerCurrencyCaretDownClick(){
+        headerPage.getHeaderCurrencyCaretDown().click();
+        return this;
+    }
+
+    public HeaderPageBL headerCurrencyChoose(String Currencies){
+        for(WebElement current : headerPage.getHeaderCurrencyList()){
+            if(current != null & current.getAttribute("name").equalsIgnoreCase(Currencies)){
+                current.click();
+                break;
+            }
+        }
+        return this;
+    }
+
+    public SearchPageBL useSearch(String product){
         headerPage.getSearch().click();
         headerPage.getSearch().clear();
         headerPage.getSearch().sendKeys(product);
         headerPage.getSearchStringSubmit().click();
-        return new SearchPage();
+        return new SearchPageBL();
     }
+
 }
