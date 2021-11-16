dda8c8866ae52227e92be4a35d1cb0bb2f0eb8b9
Sviatoslav507 <92598985+Sviatoslav507@users.noreply.github.com>
2021-11-16 15:24:47 +0200

    NewUpdate

--- src/main/java/com/opencart/pages/CartPage.java
+++ src/main/java/com/opencart/pages/CartPage.java
@@ -1,38 +1,19 @@
 package com.opencart.pages;
 
+import com.opencart.pages.containers.CartProductContainer;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.ExpectedConditions;
+
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class CartPage extends BasePage {
 
     @FindBy(xpath = "//form//table[@class = 'table table-bordered']//tbody/tr")
     private List<WebElement> cartProducts;
 
-    @FindBy(xpath = "//div[@class = 'input-group btn-block']//input[@value]")
-    private WebElement cartPageProductQuantity;
-
-    @FindBy(xpath = "//button[@type = 'submit']")
-    private WebElement cartUpdate;
-
-    @FindBy(xpath = "//table[@class = 'table table-bordered']//tbody//td[@class = 'text-left']//a")
-    private List<WebElement> cartProductsName;
-
-    @FindBy(xpath = "//button[@class = 'btn btn-danger']")
-    private WebElement cartRemove;
-
-    public List<WebElement> getCartProductsName() {
-        wait.until(ExpectedConditions.visibilityOfAllElements(cartProductsName));
-        return cartProductsName;
+    public List<CartProductContainer> getProducts() {
+        return cartProducts.stream().map(CartProductContainer::new).collect(Collectors.toList());
     }
-
-    public WebElement getCartRemove() { return cartRemove; }
-
-    public List<WebElement> getCartProducts() { return cartProducts; }
-
-    public WebElement getCartPageQuantity(){ return cartPageProductQuantity; }
-
-    public WebElement getCartUpdate() { return cartUpdate; }
 }
