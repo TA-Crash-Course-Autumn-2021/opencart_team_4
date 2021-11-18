c9b2cbaca4529d46be83b93746c91dde9b796237
Sviatoslav507 <92598985+Sviatoslav507@users.noreply.github.com>
2021-11-18 18:19:55 +0200

    Login page update

--- src/main/java/com/opencart/pages/LoginPage.java
+++ src/main/java/com/opencart/pages/LoginPage.java
@@ -1,4 +1,24 @@
 package com.opencart.pages;
 
+import lombok.Getter;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+    @Getter
 public class LoginPage {
+
+    @FindBy(xpath = "//a[text() = 'Continue']")
+    private WebElement loginPageContinueCreateAccount;
+
+    @FindBy(xpath = "//input[@id = 'input-email']")
+    private WebElement loginPageSetEmail;
+
+    @FindBy(xpath = "//input[@id = 'input-password']")
+    private WebElement loginPageSetPassword;
+
+    @FindBy(xpath = "//div[@class = 'form-group']/a")
+    private WebElement loginPageForgottenPass;
+
+    @FindBy(xpath = "//input[@type = 'submit']")
+    private WebElement loginPageLoginButton;
 }
