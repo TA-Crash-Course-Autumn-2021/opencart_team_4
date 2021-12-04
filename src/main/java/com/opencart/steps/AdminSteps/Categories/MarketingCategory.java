3c92835b0e05a7b94460f77ad8697ff4c3ee8108
AnnaGingerTea <90789719+AnnaGingerTea@users.noreply.github.com>
2021-12-04 18:28:49 +0200

    Marketing Category

---
+++ src/main/java/com/opencart/steps/AdminSteps/Categories/MarketingCategory.java
@@ -0,0 +1,16 @@
+package com.opencart.steps.AdminSteps.Categories;
+
+import com.opencart.pages.AdminPages.AdminNavigationBar;
+import com.opencart.steps.AdminSteps.AdminCouponsPageBL;
+
+public class MarketingCategory {
+    private AdminNavigationBar adminNavigationBar;
+
+    public MarketingCategory() {adminNavigationBar = new AdminNavigationBar(); }
+    
+    public AdminCouponsPageBL clickOnCoupons(){
+        adminNavigationBar.getCoupons().click();
+        return new AdminCouponsPageBL();
+    }
+
+}
