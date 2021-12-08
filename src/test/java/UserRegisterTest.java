504a323b87991e42d9c04282a14d1c391f2e8781
Svitoslav Vilzhanowskyi <92598985+Sviatoslav507@users.noreply.github.com>
2021-12-08 14:27:35 +0200

    Delete src/test/java directory

--- src/test/java/UserRegisterTest.java
+++
@@ -1,9 +0,0 @@
-import com.opencart.navigation.Navigation;
-import com.opencart.steps.MainPageBL;
-import com.opencart.steps.RegisterPageBL;
-import org.testng.annotations.Test;
-
-import static com.opencart.enums.URLs.BASE_URL;
-
-public class UserRegisterTest extends BaseTest {
-    }
